345e86b401fc2f946eedf451e62d1185689ac6ef
jjoonior <jjoonior97@gmail.com>
2024-08-20 17:10:11 +0900

    Feat: application.properties 시크릿 적용 테스트

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
+  @Value("${test.key}")
+  private String testKey;
+
   @RequestMapping("/")
   public String home() {
-    return "Hello Docker World@";
+    return "Hello Docker World : " + testKey;
   }
 
 	public static void main(String[] args) {
